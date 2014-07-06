c526d480cfd0a2a0a50f53b6ac4c4519c2558b6a
Amy Hanlon <amyehanlon@gmail.com>
2014-07-06 14:18:51 -0400

    fix date and other articles formatting

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,7 @@ SITEURL = 'http://amygdalama.github.io'
 TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
+DEFAULT_DATE_FORMAT = ('%B %d %Y')
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
